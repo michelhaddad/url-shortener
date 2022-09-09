08f66b4a8436c0a83f7239309d1a96379d9af79e
michelhaddad <michhadd1@gmail.com>
2022-09-09 18:09:54 +0200

    Move redis connection properties into application.properties

--- key-generator/src/main/java/io/michel/keygenerator/config/RedisConfiguration.java
+++ key-generator/src/main/java/io/michel/keygenerator/config/RedisConfiguration.java
@@ -1,20 +1,39 @@
 package io.michel.keygenerator.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
+@ConfigurationProperties("redis.service")
 public class RedisConfiguration {
+
+    private String host;
+    private int port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        // TODO: Add connection properties in file
-        JedisConnectionFactory jedisConFactory
-                = new JedisConnectionFactory();
-        jedisConFactory.setHostName("redis");
-        jedisConFactory.setPort(6379);
-        return jedisConFactory;
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(getHost(), getPort());
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
